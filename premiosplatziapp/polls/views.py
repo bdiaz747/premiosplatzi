42874d149749c7c2530719f56a92d2b32d3899cd
Brahian Diaz G <brahian.diazgarcia@gmail.com>
2023-06-07 21:26:48 -0500

    Creando el superusuario y las vistas de polls

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -4,7 +4,16 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hola, Mundo !!")
+    return HttpResponse("Estas en la página principal de Premios Platzi App")
+
+def detail(request, question_id):
+    return HttpResponse(f"Estas viendo la pregunta número {question_id}")
+
+def results(request, question_id):
+    return HttpResponse(f"Estas viendo los resultados de la pregunta número {question_id}")
+
+def vote(request, question_id):
+    return HttpResponse(f"Estas votando a la pregunta número {question_id}")
 
 
 
