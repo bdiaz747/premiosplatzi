9c61277348791c470a9b22bb130737fee13c6934
Brahian Diaz G <brahian.diazgarcia@gmail.com>
2023-06-10 23:36:31 -0500

    Implementando generic views en la aplicación

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,28 +1,46 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
+
 
 from . models import Question, Choice
 
 
 # Create your views here.
-def index(request):
-    latest_question_list = Question.objects.all()
-    return render(request,"polls/index.html", {
-        "latest_question_list": latest_question_list
-    })
+# def index(request):
+#     latest_question_list = Question.objects.all()
+#     return render(request,"polls/index.html", {
+#         "latest_question_list": latest_question_list
+#     })
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html",{
-        "question": question
-    } )
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, "polls/detail.html",{
+#         "question": question
+#     } )
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request,"polls/results.html",{
-            "question": question
-        })
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request,"polls/results.html",{
+#             "question": question
+#         })
+
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"  # Plantilla utilizada para renderizar la vista
+    context_object_name = "latest_question_list"  # Nombre del objeto en el contexto de la plantilla
+
+    def get_queryset(self):
+        return Question.objects.order_by("-pub_date")[:5]  # Consulta para obtener todas las preguntas
+
+class DetailView(generic.DetailView):
+    model = Question  # Modelo utilizado para la vista
+    template_name = "polls/detail.html"  # Plantilla utilizada para renderizar la vista de detalle
+
+class ResultsView(generic.DetailView):
+    model = Question  # Modelo utilizado para la vista
+    template_name = "polls/results.html"  # Plantilla utilizada para renderizar la vista de resultados
+
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
