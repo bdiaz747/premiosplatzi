ac923ee94edf062f82412491ecbb427d0709a11a
Brahian Diaz G <brahian.diazgarcia@gmail.com>
2023-06-11 21:39:15 -0500

    Escribiendo nuestro primer test

--- premiosplatziapp/polls/models.py
+++ premiosplatziapp/polls/models.py
@@ -11,15 +11,25 @@ class Question(models.Model):
         return self.question_test
     
     def was_published_recently(self):
-        return self.pub_date >=timezone.now - datetime.timedelta(days=1)
+    # Verifica si la fecha de publicación es reciente con un día atras a la fecha actual
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
         
 
 class Choice(models.Model):
+    # Clase que representa una opción dentro de un modelo de encuesta en Django   
     questions = models.ForeignKey(Question, on_delete=models.CASCADE)
+    # Campo ForeignKey que establece una relación de clave externa con el modelo Question.
+    # Cada opción pertenece a una pregunta específica.
     choice_test = models.CharField(max_length=200)
+    # Campo CharField que almacena el texto de la opción.
+    # El parámetro max_length=200 indica que la longitud máxima del texto es de 200 caracteres.
     votes = models.IntegerField(default=0)
-    
+    # Campo IntegerField que almacena la cantidad de votos que ha recibido la opción.
+    # El parámetro default=0 establece el valor predeterminado de votos en 0.
     def __str__(self):
+        # Método especial que devuelve una representación legible de la opción.
         return self.choice_test
+
 
 
