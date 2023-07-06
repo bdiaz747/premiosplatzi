6afd90f9b49ab7015b37bd638e6f16a3f5cd723c
Brahian Diaz G <brahian.diazgarcia@gmail.com>
2023-07-05 21:33:57 -0500

    Editando la interfaz del admin

--- premiosplatziapp/polls/admin.py
+++ premiosplatziapp/polls/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
+
+class ChoiceInLine(admin.StackedInline):
+    model = Choice
+    extra = 3
+    
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ["pub_date", "question_test"]
+    inlines = [ChoiceInLine] 
+    
 
 
-admin.site.register(Question)
+admin.site.register(Question,QuestionAdmin)
 
 
